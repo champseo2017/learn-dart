4e0b41683d8114aca9a27bef1e120cf248e7f17e
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2023-08-20 14:15:39 +0700

    การสร้างเลขสุ่ม

--- example1-1.dart
+++ example1-1.dart
@@ -1,14 +1,28 @@
-import 'package:intl/intl.dart';
-
 /* 
-การจัดรูปแบบตัวเลข
+การสร้างเลขสุ่ม
+lib dart:math
+- nextInt(max) สร้างเลขสุ่มจำนวนเต็มระหว่าง 0 - (max - 1) เช่น
+nextInt(100) => จะได้ค่าระหว่าง 0 - 99
+
+- nextDouble() สร้างเลขสุ่มชนิดเลขทศนิยมระหว่าง 0 - 1
+
+- nextBool() จะได้ true หรือ false อย่างใดอย่างหนึ่ง
+
+กรณี nextInt() ถ้าอยากได้ค่า ต่ำสุด - สูงสุด
+min + nextInt(max - min + 1);
 
 */
 
+import 'dart:math';
+
 void main() {
-  var fmt = NumberFormat();
-  int num = 1234567;
-  print(fmt.format(num));
-  print(fmt.format(9876654.321));
-  print(fmt.format(-1081009));
+  var rand = Random();
+
+  print(rand.nextInt(10));
+  print(rand.nextDouble());
+  print(rand.nextBool());
+
+  int min = 1, max = 20;
+  int r1_20 = min + rand.nextInt(max - min + 1);
+  print(r1_20);
 }
