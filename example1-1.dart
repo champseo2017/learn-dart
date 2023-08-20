209be190b5ddb6e91ee3dc8fb23f2d3672a30550
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2023-08-20 13:56:51 +0700

    การจัดรูปแบบตัวเลข

--- example1-1.dart
+++ example1-1.dart
@@ -1,7 +1,14 @@
-import 'dart:io';
+import 'package:intl/intl.dart';
+
+/* 
+การจัดรูปแบบตัวเลข
+
+*/
 
 void main() {
-  int e = 100;
-  e += e++;
-  print(e);
+  var fmt = NumberFormat();
+  int num = 1234567;
+  print(fmt.format(num));
+  print(fmt.format(9876654.321));
+  print(fmt.format(-1081009));
 }
